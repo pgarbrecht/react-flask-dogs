e57084cc99f368f65834e802eb37634eb0947ec5
pgarbrecht <philgarbrecht@gmail.com>
2022-10-24 16:31:51 -0500

    DogContainer component created, appears on app.js

--- dogs-frontend/src/App.js
+++ dogs-frontend/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
+import DogContainer from './components/DogContainer'
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>app page</h1>
+    <Router>
+        <Routes>
+            <Route 
+                path='/'
+                element={<DogContainer
+                
+                />}
+            />
+        </Routes>
+    </Router>
+    </>
+  )
+}
 }
 
 export default App;
---
+++ dogs-frontend/src/components/DogContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../index.css'
+
+class DogContainer extends Component {
+    render() {
+        return(
+            <h1>Dog Container</h1>
+        )
+    }
+}
+
+export default DogContainer

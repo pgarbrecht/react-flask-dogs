7b959995e5aa08da0488539f6f89fe908d17dc0c
pgarbrecht <philgarbrecht@gmail.com>
2022-10-24 14:35:47 -0500

    can add dog in postman

--- dogs-backend/resources/dogs.py
+++ dogs-backend/resources/dogs.py
@@ -2,14 +2,24 @@
 import models
 
 #this is similar to router, it creates a controller
-from flask import Blueprint
+# request let's us get data from client's request and send it to the global object. 
+# we can use this request data to get the json or form data
+from flask import Blueprint, request
+
 
 #create our blueprint
 #first arg is the blueprint name
 #second arg is its import_name
 dogs = Blueprint('dogs', 'dogs')
 
-@dogs.route('/')
+@dogs.route('/', methods=['GET'])
 def dogs_index():
     return "dogs resource working"
-    
+
+# dog create route
+@dogs.route('/', methods=['POST'])
+def create_dogs():
+    #this is like req.body in express
+    payload = request.get_json() 
+    print(payload)
+    return "you hit the create route -- check terminal"

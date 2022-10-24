8d94c9a67403c6dc189b8cb44979fb9efcba7cc2
pgarbrecht <philgarbrecht@gmail.com>
2022-10-24 15:00:55 -0500

    api type response after creating dog

--- dogs-backend/resources/dogs.py
+++ dogs-backend/resources/dogs.py
@@ -4,8 +4,10 @@ import models
 #this is similar to router, it creates a controller
 # request let's us get data from client's request and send it to the global object. 
 # we can use this request data to get the json or form data
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
+# additional tools from peewee
+from playhouse.shortcuts import model_to_dict
 
 #create our blueprint
 #first arg is the blueprint name
@@ -23,5 +25,17 @@ def create_dogs():
     payload = request.get_json() 
     print(payload)
     new_dog = models.Dog.create(name=payload['name'], age=payload['age'], breed=payload['breed'])
-    print(new_dog)
-    return "you hit the create route -- check terminal"
+    print(new_dog) #prints the id -- check sqlite3 to see the data
+    #this can be useful to get better info
+    print(new_dog.__dict__)
+    #this shows everything that comes with model class -- all methods and attributes
+    print(dir(new_dog))
+
+    #you cant just jsonify new_dog, because it's not a dictionary, instead we convert model to dict
+    dog_dict = model_to_dict(new_dog)
+
+    return jsonify(
+        data=dog_dict,
+        message='Successfully created dog!',
+        status=201
+    ), 201

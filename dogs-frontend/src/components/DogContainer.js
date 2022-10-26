e07deb429b8f254574acd83c8298bf2eab00c645
pgarbrecht <philgarbrecht@gmail.com>
2022-10-26 11:18:30 -0500

    edit form now works for multiple changes

--- dogs-frontend/src/components/DogContainer.js
+++ dogs-frontend/src/components/DogContainer.js
@@ -32,8 +32,14 @@ class DogContainer extends Component {
     }
 
     handleEditChange = (e) => {
+        // this.setState({
+        //     dogCurrentlyBeingEdited: {
+        //         [e.target.id]: e.target.value
+        //     }
+        // })
         this.setState({
             dogCurrentlyBeingEdited: {
+                ...this.state.dogCurrentlyBeingEdited,
                 [e.target.id]: e.target.value
             }
         })
